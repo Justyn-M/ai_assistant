4015570d0c7cc8e2bfde8773f045716db4baaa3e
Justyn Mahen <justyn2s04mahen23@gmail.com>
2025-02-06 19:36:41 +0800

    Updated rss_reader to allow multiple RSS feeds

--- backend/rss_reader.py
+++ backend/rss_reader.py
@@ -20,10 +20,45 @@ def get_rss_feed(url):
 
 # Functionality Testing
 if __name__ == '__main__':
-    test_url = "https://feeds.skynews.com/feeds/rss/home.xml"
-    items = get_rss_feed(test_url)
-    for item in items:
-        print("Title:", item['title'])
-        print("Link:", item['link'])
-        print("Published:", item['published'])
-        print("-" * 40)
+    print('Which news provider would you like to access? (SkyNews, 9news.com)')
+    usrinput = input().strip().lower() #convert to lowercase
+    
+    if usrinput == 'skynews':
+        print('What news section would you like to read? (Home, World, Business, Technology, Politics)')
+        newsoption = input().strip().lower()
+
+        if newsoption == 'home':
+            test_url = "https://feeds.skynews.com/feeds/rss/home.xml"
+
+        elif newsoption == 'world':
+            test_url = "https://feeds.skynews.com/feeds/rss/world.xml"
+
+        elif newsoption == 'business':
+            test_url = "https://feeds.skynews.com/feeds/rss/business.xml"
+            
+        elif newsoption == 'technology':
+            test_url = "https://feeds.skynews.com/feeds/rss/technology.xml"
+            
+        elif newsoption == 'politics':
+            test_url = "https://feeds.skynews.com/feeds/rss/politics.xml"
+        else:
+            print("Invalid Input")
+        
+        items = get_rss_feed(test_url)
+        for item in items:
+            print("Title:", item['title'])
+            print("Link:", item['link'])
+            print("Published:", item['published'])
+            print("-" * 40)
+
+
+    elif usrinput == '9news.com':
+        test_url = "https://www.9news.com/feeds/syndication/rss/news"
+        items = get_rss_feed(test_url)
+        for item in items:
+            print("Title:", item['title'])
+            print("Link:", item['link'])
+            print("Published:", item['published'])
+            print("-" * 40)
+    else:
+        print("Invalid Input")
